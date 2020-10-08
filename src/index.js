cc743519047ec50ddd94427fed587b50fe40469d
lavlife06 <lav7990192369@gmail.com>
2020-10-08 20:25:49 +0530

    setup index.js for webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-alert("HELLO FROM WEBPACK!");
+import { ComponentService } from "./app/component.service";
+import { AlertService } from "./app/alert.service";
+import { run } from "./app/app";
+
+const alertService = new AlertService();
+const componentService = new ComponentService();
+
+run(alertService, componentService);
